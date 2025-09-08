25413cb9aa6dc761014ed08e70e8a6ce42b36c83
Буланов Александр <alexbulanovgoodmail@gmail.com>
2025-09-08 17:48:16 +0300

    wip: Обновлена разметка страницы и компоненты About

--- app/components/AboutUs.vue
+++ app/components/AboutUs.vue
@@ -1,15 +1,140 @@
+<script setup lang="ts">
+import type { StructuredTextDocument } from 'vue-datocms'
+import { StructuredText as DatocmsStructuredText } from 'vue-datocms'
+import { useDatoRender } from '~/composables/useDatoRender'
+
+interface Props {
+	content: StructuredTextDocument
+}
+
+defineProps<Props>()
+
+const { renderBlock } = useDatoRender()
+</script>
+
 <template>
 	<section class="about-us">
-		<div class="container">Story</div>
+		<div class="container">
+			<div class="about-us__wrapper">
+				<div class="about-us__blocks">
+					<DatocmsStructuredText
+						class="about-us__block user-content"
+						:data="content"
+						:render-block="renderBlock"
+					/>
+				</div>
+			</div>
+		</div>
 	</section>
 </template>
 
 <style scoped lang="scss">
 .about-us {
-	margin: 50px 0;
+	margin: 40px 0;
 
-	@media screen and (min-width: $md) {
+	@media screen and (min-width: $lg) {
 		margin: 160px 0;
+	}
+
+	&__wrapper {
+		margin: 0 auto;
+
+		@media screen and (min-width: $lg) {
+			max-width: calc(100% - 400px);
+		}
+	}
+
+	&__block {
+		position: relative;
+		text-align: center;
+
+		@include typo-h2;
+	}
+
+	:deep(p:not(:last-child)) {
+		margin-bottom: 50px;
+
+		@include user-content;
+	}
+
+	:deep(.img-container) {
+		position: relative;
+		z-index: 10;
+		margin: 0 auto;
+		width: 100%;
+		max-width: 375px;
+		padding: 0 60px;
+
+		&::after {
+			content: '';
+			display: block;
+			width: 100%;
+			padding-top: calc(100%);
+		}
+
+		@media screen and (min-width: $lg) {
+			padding: 0 120px;
+		}
+	}
+
+	:deep(.img-container:not(:first-child)) {
+		margin-top: 50px;
+	}
+
+	:deep(.img-container:not(:last-child)) {
+		margin-bottom: 50px;
+	}
+
+	:deep(.img-container img) {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		border-radius: 100%;
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
+		transform-origin: 50% 50%;
+		transform: translate(-50%, -50%);
+		filter: drop-shadow(0 4px 4px rgb(0 0 0 / 25%));
+	}
+
+	:deep(.img-container:first-of-type) {
+		@media screen and (min-width: $lg) {
+			position: absolute;
+			left: 0;
+			top: 0;
+			z-index: -1;
+			width: 200px;
+			height: 200px;
+			padding: 0;
+			transform: translate(-100%, -50%);
+		}
+	}
+
+	:deep(.img-container:nth-of-type(2)) {
+		@media screen and (min-width: $lg) {
+			position: absolute;
+			right: 0;
+			bottom: 50%;
+			z-index: -1;
+			width: 200px;
+			height: 200px;
+			padding: 0;
+			transform: translate(100%, 50%);
+		}
+	}
+
+	:deep(.img-container:last-of-type) {
+		@media screen and (min-width: $lg) {
+			position: absolute;
+			left: 0;
+			bottom: 0;
+			z-index: -1;
+			width: 200px;
+			height: 200px;
+			padding: 0;
+			transform: translate(-100%, 50%);
+		}
 	}
 }
 </style>
--- app/pages/about/index.vue
+++ app/pages/about/index.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { SeoMetaTagType, StructuredTextDocument } from 'vue-datocms'
+import { useErrorHandler } from '~/composables/useErrorHandler'
+import { toHead } from 'vue-datocms'
+
+interface PageData {
+	about: {
+		_seoMetaTags: SeoMetaTagType[]
+		content: StructuredTextDocument
+	}
+}
+
+const headers = useRequestHeaders()
+const { data, error } = await useFetch<PageData>('/api/about/', {
+	headers
+})
+
+useErrorHandler(error)
+
+if (data.value) {
+	const { _seoMetaTags } = data.value.about
+	useHead(() => {
+		return {
+			...toHead(_seoMetaTags || [])
+		}
+	})
+}
+</script>
 
 <template>
-	<main>
-		<AboutUs />
+	<main v-if="data">
+		<AboutUs :content="data?.about.content" />
 	</main>
 </template>
