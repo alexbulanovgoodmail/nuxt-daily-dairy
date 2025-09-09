13a9e15453e1dfa11ac5d8f1af19eafd0c2acd11
Буланов Александр <alexbulanovgoodmail@gmail.com>
2025-09-09 11:52:31 +0300

    wip: Добавлен плагин инициализации

---
+++ app/plugins/init.server.ts
@@ -0,0 +1,9 @@
+import type { Pinia } from 'pinia'
+import { storeLayout } from '~/stores/storeLayout'
+
+export default defineNuxtPlugin(async nuxtApp => {
+	const pinia = nuxtApp.$pinia as Pinia
+	const layout = storeLayout(pinia)
+
+	await Promise.all([layout.fetch()])
+})
