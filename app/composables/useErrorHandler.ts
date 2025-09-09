6ce4d3a455af415f1f4a339d467a3449df2f5979
Буланов Александр <alexbulanovgoodmail@gmail.com>
2025-09-09 11:05:43 +0300

    wip: Почищены комментарии и логи

--- app/composables/useErrorHandler.ts
+++ app/composables/useErrorHandler.ts
@@ -2,8 +2,6 @@ import { watch } from 'vue'
 
 export function useErrorHandler(error: Ref<any>) {
 	if (import.meta.server && error.value) {
-		console.log('eeee server >>>', error)
-		// SSR - сразу выбрасываем ошибку, чтобы Nuxt показал error page
 		throw createError({
 			statusCode: error.value.statusCode || 500,
 			statusMessage: error.value.statusMessage || 'Internal Server Error',
@@ -12,13 +10,10 @@ export function useErrorHandler(error: Ref<any>) {
 	}
 
 	if (import.meta.client) {
-		console.log('eeee client >>>', error)
-		// CSR - реагируем асинхронно
 		watch(
 			error,
 			err => {
 				if (err) {
-					// Показ уведомления (опционально)
 					showError({
 						statusCode: err.statusCode || 500,
 						statusMessage: err.statusMessage || 'Internal Server Error'
